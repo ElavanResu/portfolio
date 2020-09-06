13e09f266f171876d7a855eea7e380aedbd0c059
shubham <shubham.navale@coradius.io>
2020-09-06 15:10:34 +0530

    Built contacts page along with forms

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '@material-ui/core/Container';
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Snackbar from '@material-ui/core/Snackbar';
+import { makeStyles, withStyles, ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  aboutDiv: {
+  contactDiv: {
     display: 'flex',
     flexDirection: 'column',
     flex: 1,
@@ -26,24 +30,244 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(4),
     color: '#3EFDD8'
   },
-  sentence: {
-    color: '#949699',
-    marginBottom: theme.spacing(2)
-  },
-  lastSentence: {
+  firstSentence: {
     color: '#949699',
   },
-  secondaryColor: {
-    color: '#3EFDD8'
+  snackbarText: {
+    color: '#3EFDD8',
+    textAlign: 'center'
+  },
+  form: {
+    marginTop: theme.spacing(3)
+  },
+  textFieldRoot: {
+    '& .MuiInputBase-root': {
+      color: '#949699',
+    },
+    '& .MuiInput-underline:before': {
+      borderBottomColor: '#949699',
+      borderBottomWidth: '2px'
+    },
+    '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
+      borderBottomColor: '#949699',
+      borderBottomWidth: '2px'
+    },
+    '& .Mui-error:after': {
+      borderBottomColor: '#f44336 !important'
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: '#3EFDD8'
+    },
+    '& .Mui-focused:not(.Mui-error)': {
+      color: '#3EFDD8'
+    },
+    '& .MuiFormLabel-root:not(.Mui-focused):not(.Mui-error)': {
+      color: '#949699'
+    }
+  },
+  submit: {
+    marginTop: theme.spacing(3)
+  },
+  snackbarRoot: {
+    '& .MuiSnackbarContent-root': {
+      justifyContent: 'center'
+    }
   }
 }))
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#3EFDD8'
+    },
+    secondary: {
+      main: '#949699'
+    }
+  }
+});
+
 const Contacts = (props) => {
-  console.log('render skills')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [subject, setSubject] = useState('')
+  const [message, setMessage] = useState('')
+  const [errors, setErrors] = useState([])
+  const [openSnack, setOpenSnack] = useState(false)
   const classes = useStyles()
+  const handleInputChange = event => {
+    event.preventDefault()
+    const target = event.target;
+    const name = target.name;
+    if (name === 'name') {
+      setName(target.value)
+    } else if (name === 'email') {
+      setEmail(target.value)
+    } else if (name === 'subject') {
+      setSubject(target.value)
+    } else if (name === 'message') {
+      setMessage(target.value)
+    }
+  }
+
+  const checkForErrors = () => {
+    let newErrorSet = [...errors]
+    const emailKey = newErrorSet.findIndex(ele => ele.key === 'email')
+    const messageKey = newErrorSet.findIndex(ele => ele.key === 'message')
+    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
+      if (emailKey === -1) {
+        newErrorSet.push({
+          key: 'email',
+          message: 'Enter valid email id'
+        })
+      }
+    } else {
+      newErrorSet = newErrorSet.filter(ele => ele.key !== 'email')
+    }
+    if (message === undefined || message === null || (message !== undefined && message.length === 0)) {
+      if (messageKey === -1) {
+        newErrorSet.push({
+          key: 'message',
+          message: 'Enter your message'
+        })
+      }
+    } else {
+      newErrorSet = newErrorSet.filter(ele => ele.key !== 'message')
+    }
+    return newErrorSet
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    event.persist()
+    const errorsList = checkForErrors()
+    if (errorsList.length > 0) {
+      console.log('got error: ', errorsList)
+      setErrors(errorsList)
+    } else {
+      console.log('name: ', name)
+      console.log('email: ', email)
+      console.log('subject: ', subject)
+      console.log('message: ', message)
+      setErrors([])
+      fetch('https://discordapp.com/api/webhooks/752084764277866578/rfttZtgOMXx8mhvr_5vr21EVLPC47JzHi7d3RhOl8q3_FEcQYVHTp06U_CooAKBwP2_y', {
+        method: 'post',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({content: `**Got new message:**
+        Name:  \`${name}\`
+        email:  \`${email}\`
+        subject:  \`${subject}\`
+        message:  \`${message}\``})
+      }).then(function(response) {
+        setOpenSnack(true)
+        setTimeout(() => setOpenSnack(false), 4000)
+        console.log('response: ',response)
+      }).catch(function(error) {
+        console.log('error in sending: ', error)
+      })
+    }
+  }
   return (
-    <div className={classes.aboutDiv}>
-      contact
+    <div onSubmit={handleSubmit} className={classes.contactDiv}>
+      <Container maxWidth='xs' id='aboutdiv'
+        classes={{
+          root: classes.rootContainer
+        }}
+      >
+        <Typography variant="h3" className={classes.title}>
+          Contact me
+        </Typography>
+        <Typography variant="body2" className={classes.firstSentence}>
+          I am always open for new opportunities. If have any questions and want to talk with me, don't hesitate to contact me using the bellow form.
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                classes={{
+                  root: classes.textFieldRoot
+                }}
+                // autoComplete="name"
+                name="name"
+                fullWidth
+                id="name"
+                label="Name"
+                onChange={handleInputChange}
+                value={name.value}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                classes={{
+                  root: classes.textFieldRoot
+                }}
+                // autoComplete="email"
+                name="email"
+                required
+                error={errors.findIndex(ele => ele.key === 'email') !== -1}
+                fullWidth
+                id="email"
+                label="Email"
+                onChange={handleInputChange}
+                value={email.value}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                classes={{
+                  root: classes.textFieldRoot
+                }}
+                // autoComplete="email"
+                name="subject"
+                fullWidth
+                id="subject"
+                label="Subject"
+                onChange={handleInputChange}
+                value={subject.value}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                classes={{
+                  root: classes.textFieldRoot
+                }}
+                // autoComplete="email"
+                name="message"
+                required
+                error={errors.findIndex(ele => ele.key === 'message') !== -1}
+                fullWidth
+                id="message"
+                label="Message"
+                onChange={handleInputChange}
+                value={message.value}
+              />
+            </Grid>
+          </Grid>
+          <ThemeProvider theme={theme}>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Dispatch
+            </Button>
+          </ThemeProvider>
+        </form>
+      </Container>
+      <Snackbar
+        open={openSnack}
+        classes={{
+          root: classes.snackbarRoot
+        }}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        message={
+          <Typography variant="body2" className={classes.snackbarText}>
+            Message Dispatched!!!
+          </Typography>
+        }
+      />
     </div>
   )
 }
